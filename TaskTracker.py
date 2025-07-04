bd693a680763589339194615122faafa337ed8cd
unknown <danielbumbara03@gmail.com>
2025-07-04 10:31:01 +0300

    Finished implementic all functionality

--- TaskTracker.py
+++ TaskTracker.py
@@ -13,6 +13,45 @@ def save_tasks(tasks):
     with open("tasks.json", "w") as f:
         json.dump(tasks, f, indent=2)
 
+def create_task(description):
+    return {
+        "id": int(datetime.now().timestamp()),
+        "description": description,
+        "status": "todo",
+        "createdAt":datetime.now().isoformat(),
+        "updatedAt": datetime.now().isoformat()
+    }
+
+def update_task(tasks, task_id, new_description):
+
+    for task in tasks:
+        if task["id"] == task_id:
+            task["description"] = new_description
+            task["updatedAt"] = datetime.now().isoformat()
+            return True
+
+    return False
+
+def delete_task(tasks, task_id):
+    for i, task in enumerate(tasks):
+        if task["id"] == task_id:
+            del tasks[i]
+            return True
+    return False
+
+def mark_task(tasks, task_id, status):
+    for task in tasks:
+        if task["id"] == task_id:
+            task["status"] = status
+            task["updatedAt"] = datetime.now().isoformat()
+            return True
+    return False
+
+def filter_tasks(tasks, status=None):
+    if status:
+        return [task for task in tasks if task["status"] == status]
+    else:
+        return tasks
 
 
 def main():
@@ -42,7 +81,43 @@ def main():
     list_parser.add_argument("status", nargs="?", choices=["todo", "in-progress", "done"], help="Filter tasks by status")
 
     args = parser.parse_args()
-    print(args)
+
+    if args.command == "add":
+        tasks = load_tasks()
+        task = create_task(args.description)
+        tasks.append(task)
+        save_tasks(tasks)
+        print(f"Task added successfully (ID: {task['id']})")
+    elif args.command == "update":
+        tasks = load_tasks()
+        if update_task(tasks, args.id, args.description):
+            save_tasks(tasks)
+            print(f"Task {args.id} updated successfully")
+        else:
+            print(f"Task {args.id} not found") 
+    elif args.command == "delete":
+        tasks = load_tasks()
+        if delete_task(tasks, args.id):
+            save_tasks(tasks)
+            print(f"Task {args.id} deleted successfully")
+        else:
+            print(f"Task {args.id} not found")
+    elif args.command == "mark":
+        tasks = load_tasks()
+        if mark_task(tasks, args.id, args.status):
+            save_tasks(tasks)
+            print(f"Task {args.id} marked as {args.status}")
+        else:
+            print(f"Task {args.id} not found")
+    elif args.command == "list":
+            tasks = load_tasks()
+            filtered_tasks = filter_tasks(tasks, args.status)
+            for task in filtered_tasks:
+                print(f"{task['id']}: {task['description']} ({task['status']})")
+    else:
+        print(args)
+
+
 
 if __name__ == "__main__":
     main()
